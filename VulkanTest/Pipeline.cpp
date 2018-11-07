63e882c4cb8a1753a290cc5c273628141655aee6
Milos Ribar <milosribar@yahoo.com>
2018-11-08 00:58:32 +0100

    Pipeline reconstruction

--- VulkanTest/Pipeline.cpp
+++ VulkanTest/Pipeline.cpp
@@ -2,11 +2,72 @@
 #include "Pipeline.h"
 
 
-Pipeline::Pipeline()
+Pipeline::Pipeline(VkDevice device)
 {
+	this->device = device;
+	util = &Util::instance();
+
+	auto vertexShaderCode = loadShader("shader.vert");
+	auto fragmentShaderCode = loadShader("shader.frag");
+
+	this->createShaderModule(vertexShaderCode, &this->vertexShader);
+	this->createShaderModule(fragmentShaderCode, &this->fragmentShader);
 }
 
 
 Pipeline::~Pipeline()
 {
+	vkDestroyShaderModule(device, this->vertexShader, nullptr);
+	vkDestroyShaderModule(device, this->fragmentShader, nullptr);
+}
+
+void Pipeline::createPipeline() {
+	VkPipelineShaderStageCreateInfo vsCreateInfo{};
+	VkPipelineShaderStageCreateInfo fsCreateInfo{};
+
+	vsCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	vsCreateInfo.module = this->vertexShader;										//Pokazivac na sam kod sejdera
+	vsCreateInfo.pName = "main";													//Koju funkciju u kodu shadera da pozovem?
+	vsCreateInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;								//Koje stanje pajplajna? Za vertex shader to je Vertex Shader Stage
+
+	fsCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	fsCreateInfo.module = this->fragmentShader;
+	fsCreateInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+	fsCreateInfo.pName = "main";
+
+	this->shaderCreationInfo[0] = vsCreateInfo;
+	this->shaderCreationInfo[1] = fsCreateInfo;
+}
+
+std::array<VkPipelineShaderStageCreateInfo, 2> Pipeline::getShaderCreationInfo()
+{
+	return this->shaderCreationInfo;
+}
+
+std::vector<char> Pipeline::loadShader(const std::string filename)
+{
+	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+	size_t fileSize = file.tellg();
+	std::vector<char> buffer(fileSize);
+
+	if (!file.is_open()) {
+		throw std::runtime_error("Failed to open a file at location: " + filename);
+	}
+	else {
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+		file.close();
+	}
+
+	return buffer;
+}
+
+VkShaderModule Pipeline::createShaderModule(const std::vector<char> code, VkShaderModule* shaderHandle) {
+	VkShaderModuleCreateInfo shaderInfo = {};
+
+	shaderInfo.codeSize = code.size();
+	shaderInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+	shaderInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+	util->ErrorCheck(vkCreateShaderModule(this->device, &shaderInfo, nullptr, shaderHandle));
 }
--- VulkanTest/Pipeline.h
+++ VulkanTest/Pipeline.h
@@ -1,8 +1,30 @@
 #pragma once
+#include <string>
+#include <fstream>
+#include <vector>
+#include <array>
+#include "PLATFORM.h"
+#include "Util.h"
+
 class Pipeline
 {
 public:
-	Pipeline();
+	Pipeline(VkDevice device);
 	~Pipeline();
+
+	void createPipeline();
+
+	std::array<VkPipelineShaderStageCreateInfo, 2> getShaderCreationInfo();
+private:
+	std::vector<char> loadShader(const std::string filename);
+	VkShaderModule createShaderModule(const std::vector<char> code, VkShaderModule* shaderHandle);			//Da, ovde ide kod iz shadera.
+
+	std::array<VkPipelineShaderStageCreateInfo, 2> shaderCreationInfo;
+
+	VkShaderModule vertexShader = VK_NULL_HANDLE;
+	VkShaderModule fragmentShader = VK_NULL_HANDLE;
+	VkDevice device = VK_NULL_HANDLE;;
+
+	Util* util = nullptr;
 };
 
